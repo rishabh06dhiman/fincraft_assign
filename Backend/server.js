fd41c9a81514894cfa59be636389315c8ab18640
rishabh dhiman <dhiman.rishabh.1998@gmail.com>
2023-06-26 20:35:19 +0530

    add process.env.PORT again

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,7 @@
 const app = require("./app");
 
 const dotenv = require("dotenv");
-const connectDatabase =  require("./database")
-const port = process.env.PORT || 6000;
+const connectDatabase =  require("./database");
 
 // config file
 dotenv.config({path:"backend/config.env"});
@@ -10,6 +9,6 @@ dotenv.config({path:"backend/config.env"});
 // connected to databse
 connectDatabase();
 
-app.listen(port, ()=>{
-    console.log(`server is working on http://localhost:${port}` )
+app.listen(process.env.PORT, ()=>{
+    console.log(`server is working on http://localhost:${process.env.PORT}` )
 })
