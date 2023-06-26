e4cc13d1a2108694e5d41542a94168bbf74ea6b9
rishabh dhiman <dhiman.rishabh.1998@gmail.com>
2023-06-26 19:57:42 +0530

    add another alter port

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 
 const dotenv = require("dotenv");
 const connectDatabase =  require("./database")
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 // config file
 dotenv.config({path:"backend/config.env"});
